1924a17032f1b6a3d390b64c7e01f9f49cc5b458
jongsung1 <105058450+jongsung1@users.noreply.github.com>
2026-06-30 09:22:35 +0900

    Update cmd_reserve.sh

--- TOOL/cmd_reserve.sh
+++ TOOL/cmd_reserve.sh
@@ -1,45 +1,38 @@
 #!/bin/bash
 
-source /idea_backup/script/default/default_conf
+###############################################
+####################config#####################
+##### 쉘 색상 정의
+GREEN='\033[0;32m'
+RESET='\033[0m'
 
+##### set TIME
+YMD="2026-07-03"
+HMS="22:30:00"
 
-#####set TIME
-echo -ne "${GREEN}YEAR : ${RESET}"
-read YEAR
+##### set command
+command(){
+	/home/markany/stop.sh all
+	shutdown -h now
+}
 
-echo -ne "${GREEN}MONTH : ${RESET}"
-read MONTH
+###############################################
+###############################################
 
-echo -ne "${GREEN}DAY : ${RESET}"
-read DAY
+NOWTIME=$(date +%s)
+UNIXTIME=$(date +%s --date "$YMD $HMS")
 
-echo -ne "${GREEN}HOUR : ${RESET}"
-read HOUR
+# 남은 시간(초) 계산
+TARGET_SLEEP=$(( UNIXTIME - NOWTIME ))
 
-echo -ne "${GREEN}MINUTE : ${RESET}"
-read MINUTE
+if [ ${TARGET_SLEEP} -gt 0 ]; then
+    echo -e "${GREEN}${YMD} ${HMS}에 실행 됩니다. (남은 시간: ${TARGET_SLEEP}초)${RESET}"
 
-echo -ne "${GREEN}SECOND : ${RESET}"
-read SECOND
+    # 설정한 시간까지 한 번에 대기 (3초마다 반복 호출하지 않음)
+    sleep ${TARGET_SLEEP}
+else
+    echo -e "${GREEN}설정 시간이 이미 지났습니다. 즉시 실행합니다.${RESET}"
+fi
 
-YMD="${YEAR}-${MONTH}-${DAY}"
-HMS="${HOUR}:${MINUTE}:${SECOND}"
-
-NOWTIME=`date +%s`
-UNIXTIME=`date +%s --date "$YMD $HMS"`
-
-echo -e "${GREEN}${YMD} ${HMS}에 실행 됩니다${RESET}"
-date
-
-######설정한 시간이 되면 명령어 실행
-while [ ${NOWTIME} -le ${UNIXTIME} ] ; do
-	sleep 3
-	######현재시간을 갱신하기 위해 생성
-	NOWTIME=`date +%s`
-done
-
-date
-
-##### command
-#echo "nohup_test" >> /home/nohup.txt
-/idea_backup/script/system_backup.sh
+##### command 함수 호출
+command
